7dc3a861f96a2a1ba9c08404a807f15a5c310dcb
Sunil Prajapati <sunilp832@gmail.com>
2025-06-24 09:44:15 +0530

    Add files via upload

---
+++ anagram.py
@@ -0,0 +1,10 @@
+from collections import Counter
+class Solution(object):
+    def isAnagram(self,s,t):
+        return Counter(s) == Counter(t)
+
+if __name__ == "__main__":
+    sol = Solution()
+    s = "listen"
+    t = "silent"
+    print(sol.isAnagram(s,t))
