92473dc1f21c5269f8ef1379e9c2bc5fbb27c187
Sunil Prajapati <sunilp832@gmail.com>
2025-09-03 21:29:22 +0530

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -2,8 +2,7 @@ def factorial(n):
     if n==0 or n==1:
         return 1
     else: return n*factorial(n-1)
-
 n = int(input("Enter Number whose factorial you want"))
-
 factorial = factorial(n)
 print(factorial)
+
